64727fbd0624e67e2d00d38fa7a56e32210af545
Arnaut Daniel <silverbeard@protonmail.com>
2021-03-16 21:24:24 -0400

    Oh the wonders of pointers to pointers

--- db.fs
+++ db.fs
@@ -1,1 +1,20 @@
 require hiredis.fs
+
+variable rc
+
+: initdb ( -- )
+    s" 127.0.0.1" 6379 redisconnect rc ! ;
+
+: smember ( item -- replyobj )
+    s" smembers " 2swap s+ rc @ rot rot rediscommand ;
+
+: printmember ( replyobj -- )
+    cr dup redisreply-len @ swap redisreply-str @ swap type ;
+
+: printmembers ( replyobj -- )
+    dup redisreply-elements @ swap redisreply-element @
+    swap 0 DO
+	dup i cells + @ printmember
+    loop drop ;
+
+    
